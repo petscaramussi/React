804e36d7e9bc012c2e28aa24ca37c3af7af64451
Peterson <44592787+petscaramussi@users.noreply.github.com>
2023-11-28 13:51:20 -0300

    end section 1

---
+++ Essentials/01-starting-project/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
+import { EXAMPLES } from './data'; 
+
+function App() {
+ const [ selectedTopic, setSelectedTopic ] = useState();
+
+  function handleSelect(selectedButton) {
+    // selected button => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}                      
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')} >Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {!selectedTopic ? <p>Please select a topic.</p> : (
+            <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+          )}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton({ children, onClick, isSelected }) {
+    return (
+    <li>
+        <button className={isSelected ? 'active' : null} onClick={onClick}>{children}</button>
+    </li>
+    );
+}
