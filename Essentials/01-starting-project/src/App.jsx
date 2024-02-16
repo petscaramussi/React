a0a59eb6da4edac5ede7415224b7864fa5a29ae3
Peterson <44592787+petscaramussi@users.noreply.github.com>
2024-02-16 11:28:20 -0300

    up

--- Essentials/01-starting-project/src/App.jsx
+++ Essentials/01-starting-project/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
-import { EXAMPLES } from './data'; 
+import { EXAMPLES } from './data';
 
 function App() {
- const [ selectedTopic, setSelectedTopic ] = useState();
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     // selected button => 'components', 'jsx', 'props', 'state'
@@ -15,13 +15,13 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}                      
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
           </ul>
         </section>
         <section id="examples">
@@ -34,16 +34,16 @@ function App() {
           </menu>
           {!selectedTopic ? <p>Please select a topic.</p> : (
             <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
           )}
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
